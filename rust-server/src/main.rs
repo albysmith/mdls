0bffc39b8a7a029a478318722f32e8892623efa2
albysmith <48844001+albysmith@users.noreply.github.com>
2020-05-20 17:58:34 -0500

    hover!

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -1,10 +1,15 @@
 use std::error::Error;
 
 use log::info;
+use lsp_types::notification::DidChangeTextDocument;
+use lsp_types::request::{GotoDefinition, HoverRequest};
 use lsp_types::{
-    request::{GotoDefinition, GotoDefinitionResponse},
-    notification::{DidChangeTextDocument},
-    InitializeParams, ServerCapabilities,
+    CodeActionOptions, CodeActionProviderCapability, CodeLensOptions, CompletionOptions,
+    DocumentOnTypeFormattingOptions, FoldingRangeProviderCapability,
+    ImplementationProviderCapability, InitializeParams, RenameOptions, RenameProviderCapability,
+    SaveOptions, SelectionRangeProviderCapability, ServerCapabilities, SignatureHelpOptions,
+    TextDocumentSyncCapability, TextDocumentSyncKind, TextDocumentSyncOptions,
+    TypeDefinitionProviderCapability, WorkDoneProgressOptions,
 };
 
 use lsp_server::{Connection, Message, Request, RequestId, Response};
@@ -20,7 +25,92 @@ fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
     let (connection, io_threads) = Connection::stdio();
 
     // Run the server and wait for the two threads to end (typically by trigger LSP Exit event).
-    let server_capabilities = serde_json::to_value(&ServerCapabilities::default()).unwrap();
+    let server_settings = ServerCapabilities {
+        text_document_sync: Some(TextDocumentSyncCapability::Options(
+            TextDocumentSyncOptions {
+                open_close: Some(true),
+                change: Some(TextDocumentSyncKind::Full),
+                will_save: None,
+                will_save_wait_until: None,
+                save: Some(SaveOptions::default()),
+            },
+        )),
+        hover_provider: Some(true),
+        completion_provider: Some(CompletionOptions {
+            resolve_provider: None,
+            trigger_characters: Some(vec![":".to_string(), ".".to_string()]),
+            work_done_progress_options: WorkDoneProgressOptions {
+                work_done_progress: None,
+            },
+        }),
+        signature_help_provider: Some(SignatureHelpOptions {
+            trigger_characters: Some(vec!["(".to_string(), ",".to_string()]),
+            retrigger_characters: None,
+            work_done_progress_options: WorkDoneProgressOptions {
+                work_done_progress: None,
+            },
+        }),
+        declaration_provider: None,
+        definition_provider: Some(true),
+        type_definition_provider: Some(TypeDefinitionProviderCapability::Simple(true)),
+        implementation_provider: Some(ImplementationProviderCapability::Simple(true)),
+        references_provider: Some(true),
+        document_highlight_provider: Some(true),
+        document_symbol_provider: Some(true),
+        workspace_symbol_provider: Some(true),
+        code_action_provider: Some(CodeActionProviderCapability::Options(CodeActionOptions {
+            // Advertise support for all built-in CodeActionKinds
+            code_action_kinds: Some(vec![
+                lsp_types::code_action_kind::EMPTY.to_string(),
+                lsp_types::code_action_kind::QUICKFIX.to_string(),
+                lsp_types::code_action_kind::REFACTOR.to_string(),
+                lsp_types::code_action_kind::REFACTOR_EXTRACT.to_string(),
+                lsp_types::code_action_kind::REFACTOR_INLINE.to_string(),
+                lsp_types::code_action_kind::REFACTOR_REWRITE.to_string(),
+                lsp_types::code_action_kind::SOURCE.to_string(),
+                lsp_types::code_action_kind::SOURCE_ORGANIZE_IMPORTS.to_string(),
+            ]),
+            work_done_progress_options: Default::default(),
+        })),
+        code_lens_provider: Some(CodeLensOptions {
+            resolve_provider: Some(true),
+        }),
+        document_formatting_provider: Some(true),
+        document_range_formatting_provider: None,
+        document_on_type_formatting_provider: Some(DocumentOnTypeFormattingOptions {
+            first_trigger_character: "=".to_string(),
+            more_trigger_character: Some(vec![".".to_string(), ">".to_string()]),
+        }),
+        selection_range_provider: Some(SelectionRangeProviderCapability::Simple(true)),
+        // semantic_highlighting: None,
+        folding_range_provider: Some(FoldingRangeProviderCapability::Simple(true)),
+        rename_provider: Some(RenameProviderCapability::Options(RenameOptions {
+            prepare_provider: Some(true),
+            work_done_progress_options: WorkDoneProgressOptions {
+                work_done_progress: None,
+            },
+        })),
+        document_link_provider: None,
+        color_provider: None,
+        execute_command_provider: None,
+        workspace: None,
+        // call_hierarchy_provider: Some(CallHierarchyServerCapability::Simple(true)),
+        // semantic_tokens_provider: Some(
+        //     SemanticTokensOptions {
+        //         legend: SemanticTokensLegend {
+        //             token_types: semantic_tokens::SUPPORTED_TYPES.to_vec(),
+        //             token_modifiers: semantic_tokens::SUPPORTED_MODIFIERS.to_vec(),
+        //         },
+
+        //         document_provider: Some(SemanticTokensDocumentProvider::Bool(true)),
+        //         range_provider: Some(true),
+        //         work_done_progress_options: Default::default(),
+        //     }
+        //     .into(),
+        // ),
+        experimental: Default::default(),
+    };
+    let server_capabilities = serde_json::to_value(&server_settings).unwrap();
     let initialization_params = connection.initialize(server_capabilities)?;
     main_loop(&connection, initialization_params)?;
     io_threads.join()?;
@@ -44,17 +134,41 @@ fn main_loop(
                     return Ok(());
                 }
                 info!("got request: {:?}", req);
-                match cast::<GotoDefinition>(req) {
+                // match cast::<GotoDefinition>(req) {
+                //     Ok((id, params)) => {
+                //         info!("got gotoDefinition request #{}: {:?}", id, params);
+                //         let result = Some(lsp_types::GotoDefinitionResponse::Array(Vec::new()));
+                //         let result = serde_json::to_value(&result).unwrap();
+                //         let resp = Response {
+                //             id,
+                //             result: Some(result),
+                //             error: None,
+                //         };
+                //         connection.sender.send(Message::Response(resp))?;
+                //         continue;
+                //     }
+                //     Err(req) => req,
+                // };
+                match cast::<HoverRequest>(req) {
                     Ok((id, params)) => {
-                        info!("got gotoDefinition request #{}: {:?}", id, params);
-                        let result = Some(GotoDefinitionResponse::Array(Vec::new()));
+                        info!("got Hover request #{}: {:?}", id, params);
+                        let result = Some(lsp_types::Hover {
+                            contents: lsp_types::HoverContents::Scalar(
+                                lsp_types::MarkedString::String("Hello World!".to_string()),
+                            ),
+                            range: None,
+                        });
                         let result = serde_json::to_value(&result).unwrap();
-                        let resp = Response { id, result: Some(result), error: None };
+                        let resp = Response {
+                            id,
+                            result: Some(result),
+                            error: None,
+                        };
                         connection.sender.send(Message::Response(resp))?;
                         continue;
                     }
-                    Err(req) => req,
-                };
+                    Err(req) => (),
+                }
                 // ...
             }
             Message::Response(resp) => {
@@ -87,6 +201,30 @@ where
     req.extract(R::METHOD)
 }
 
+// no_run
+// Content-Length: 85
 
+// {"jsonrpc": "2.0", "method": "initialize", "id": 1, "params": {"capabilities": {}}}
 
+// This will respond with a server response. Then send it a `initialized` notification which will
+// have no response.
 
+// no_run
+// Content-Length: 59
+
+// {"jsonrpc": "2.0", "method": "initialized", "params": {}}
+
+// Once these two are sent, then we enter the main loop of the server. The only request this
+// example can handle is `gotoDefinition`:
+
+// no_run
+// Content-Length: 159
+
+// {"jsonrpc": "2.0", "method": "textDocument/definition", "id": 2, "params": {"textDocument": {"uri": "file://temp"}, "position": {"line": 1, "character": 1}}}
+
+// To finish up without errors, send a shutdown request:
+
+// no_run
+// Content-Length: 67
+
+// {"jsonrpc": "2.0", "method": "shutdown", "id": 3, "params": null}
