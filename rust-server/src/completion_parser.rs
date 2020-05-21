b207a96192fad0a905e0891d6aceda43f7d4d325
mewosmith <48843615+mewosmith@users.noreply.github.com>
2020-05-21 08:24:43 -0500

    slightly better simple

--- rust-server/src/completion_parser.rs
+++ rust-server/src/completion_parser.rs
@@ -2,6 +2,8 @@ use log::info;
 use lsp_types::{CompletionItem, CompletionParams};
 use roxmltree::*;
 use std::fs;
+
+
 pub fn simple_complete(params: CompletionParams) -> Vec<CompletionItem> {
     let mut namespace = vec![];
     let error_string = format!(
@@ -23,8 +25,14 @@ pub fn simple_complete(params: CompletionParams) -> Vec<CompletionItem> {
             .unwrap(),
     )
     .unwrap();
+    let line = string
+        .lines()
+        .nth(params.text_document_position.position.line as usize + 1)
+        .expect("line iterator died");
+    let byte_position = string.find(line).expect("ok this line should be here");
+    info!("byte position{}", byte_position);
     let doc = roxmltree::Document::parse(&string).expect("file isnt xml");
-    for node in doc.descendants() {
+    for (i, node) in doc.descendants().enumerate() {
         if node.tag_name().name() == "cue" {
             if let Some(n) = node.attribute("namespace") {
                 if n == "this" {
@@ -46,11 +54,28 @@ pub fn simple_complete(params: CompletionParams) -> Vec<CompletionItem> {
                 }
             }
         }
-        let pos = doc.text_pos_at(node.range().start);
-        if pos.row as u64 == params.text_document_position.position.line {
-            break;
+        if (node.tag_name().name() != "mdscript")
+            & (node.tag_name().name() != "cues")
+            & (node.tag_name().name() != "cue")
+            & (node.tag_name().name() != "actions")
+            & (node.tag_name().name() != "")
+        {
+            info!(
+                "node.range():{:?} name: {:?}",
+                node.range(),
+                node.tag_name().name()
+            );
+            if node.range().end >= byte_position {
+                info!("loop broken ");
+                break;
+            }
         }
+
+        // let pos = doc.text_pos_at(node.range().starat);
+        // if pos.row as u64 == params.text_document_position.position.line {
+        // }
     }
+
     namespace
 }
 pub fn parent_complete(params: CompletionParams) -> Vec<CompletionItem> {
