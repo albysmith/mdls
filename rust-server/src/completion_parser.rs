73344326f4784089fb0c46ba196cde24d04ceb55
mewosmith <48843615+mewosmith@users.noreply.github.com>
2020-05-21 16:03:56 -0500

    matchy

--- rust-server/src/completion_parser.rs
+++ rust-server/src/completion_parser.rs
@@ -4,6 +4,59 @@ use std::fs;
 
 pub fn simple_complete(params: CompletionParams) -> Vec<CompletionItem> {
     let mut namespace = vec![];
+
+    let (byte_position, string) = get_node_and_string(params);
+
+    let doc = roxmltree::Document::parse(&string).expect("file isnt xml");
+    for (i, node) in doc.descendants().enumerate() {
+        //filter against nodes that contain the whole fucking file in their range....
+        match node.tag_name().name() {
+            "mdscript" | "cues" | "cue" | "conditions" | "delay" | "actions" | "" => (),
+            _ => {
+                if node.range().end >= byte_position {
+                    info!(
+                        "end node range:{} byte_positon: {} \n\n node name: {}",
+                        node.range().end,
+                        byte_position,
+                        node.tag_name().name()
+                    );
+                    for ancestor in node.ancestors() {
+                        info!("ancestor {}", ancestor.tag_name().name());
+                        for f_child in ancestor.first_children() {
+                            info!("f_child {}", f_child.tag_name().name());
+                            match f_child.tag_name().name() {
+                                "actions" => {
+                                    for actions_child in f_child.children() {
+                                        info!("actions_child {}", actions_child.tag_name().name());
+                                        if let Some(attr) = actions_child.attribute("name") {
+                                            info!("COMPLETION ADDED {}", attr);
+                                            add_completion_item(&mut namespace, attr);
+                                        }
+                                    }
+                                }
+                                _ => (),
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    namespace
+}
+fn add_completion_item(namespace: &mut Vec<CompletionItem>, attr: &str) {
+    let item = CompletionItem {
+        label: attr.to_owned(),
+        ..CompletionItem::default()
+    };
+    if !namespace.contains(&item) {
+        namespace.push(item);
+    }
+}
+
+fn get_node_and_string(params: CompletionParams) -> (usize, String) {
     let string = fs::read_to_string(
         params
             .text_document_position
@@ -15,78 +68,8 @@ pub fn simple_complete(params: CompletionParams) -> Vec<CompletionItem> {
     .unwrap();
     let line = string
         .lines()
-        .nth(params.text_document_position.position.line as usize + 1)
+        .nth(params.text_document_position.position.line as usize)
         .expect("line iterator died");
     let byte_position = string.find(line).expect("ok this line should be here");
-    info!("byte position{}", byte_position);
-    let doc = roxmltree::Document::parse(&string).expect("file isnt xml");
-
-    for (i, node) in doc.descendants().enumerate() {
-        if (node.tag_name().name() != "mdscript")
-            & (node.tag_name().name() != "cues")
-            & (node.tag_name().name() != "cue")
-            & (node.tag_name().name() != "actions")
-            & (node.tag_name().name() != "")
-        {
-            if node.range().end >= byte_position {
-                info!("NODE FOUND!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-                for anc in node.ancestors() {
-                    info!("ancestor {:?}", anc.tag_name().name());
-                    if (anc.tag_name().name() != "mdscript")
-                        & (anc.tag_name().name() != "cues")
-                        & (anc.tag_name().name() != "cue")
-                    {
-                        for child in anc.descendants() {
-                            if let Some(attr) = child.attribute("name") {
-                                let item = CompletionItem {
-                                    label: attr.to_owned(),
-                                    ..CompletionItem::default()
-                                };
-                                if !namespace.contains(&item) {
-                                    namespace.push(item);
-                                }
-                            }
-                        }
-                    }
-                    if let Some(attr) = anc.attribute("name") {
-                        let item = CompletionItem {
-                            label: attr.to_owned(),
-                            ..CompletionItem::default()
-                        };
-                        if !namespace.contains(&item) {
-                            namespace.push(item);
-                        }
-                    }
-                }
-                break;
-            }
-        }
-    }
-
-    namespace
-}
-pub fn parent_complete(params: CompletionParams) -> Vec<CompletionItem> {
-    let mut namespace = vec![];
-    let error_string = format!(
-        "uri conversion is fucked {:#?}",
-        params
-            .text_document_position
-            .text_document
-            .uri
-            .to_file_path()
-            .unwrap()
-    );
-
-    let string = fs::read_to_string(
-        params
-            .text_document_position
-            .text_document
-            .uri
-            .to_file_path()
-            .unwrap(),
-    )
-    .unwrap();
-    let doc = roxmltree::Document::parse(&string).expect("file isnt xml");
-
-    namespace
+    (byte_position, string)
 }
