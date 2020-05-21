cc2ea18510301fd2a623fa3628e6fb4bc22c8339
mewosmith <48843615+mewosmith@users.noreply.github.com>
2020-05-21 08:48:05 -0500

    much more correct simple

--- rust-server/src/completion_parser.rs
+++ rust-server/src/completion_parser.rs
@@ -1,21 +1,9 @@
 use log::info;
 use lsp_types::{CompletionItem, CompletionParams};
-use roxmltree::*;
 use std::fs;
-
 
 pub fn simple_complete(params: CompletionParams) -> Vec<CompletionItem> {
     let mut namespace = vec![];
-    let error_string = format!(
-        "uri conversion is fucked {:#?}",
-        params
-            .text_document_position
-            .text_document
-            .uri
-            .to_file_path()
-            .unwrap()
-    );
-
     let string = fs::read_to_string(
         params
             .text_document_position
@@ -32,17 +20,35 @@ pub fn simple_complete(params: CompletionParams) -> Vec<CompletionItem> {
     let byte_position = string.find(line).expect("ok this line should be here");
     info!("byte position{}", byte_position);
     let doc = roxmltree::Document::parse(&string).expect("file isnt xml");
+
     for (i, node) in doc.descendants().enumerate() {
-        if node.tag_name().name() == "cue" {
-            if let Some(n) = node.attribute("namespace") {
-                if n == "this" {
-                    namespace.clear()
-                }
-            }
-        } else {
-            if let Some(n) = node.parent() {
-                if n.tag_name().name() == "actions" {
-                    if let Some(attr) = node.attribute("name") {
+        if (node.tag_name().name() != "mdscript")
+            & (node.tag_name().name() != "cues")
+            & (node.tag_name().name() != "cue")
+            & (node.tag_name().name() != "actions")
+            & (node.tag_name().name() != "")
+        {
+            if node.range().end >= byte_position {
+                info!("NODE FOUND!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                for anc in node.ancestors() {
+                    info!("ancestor {:?}", anc.tag_name().name());
+                    if (anc.tag_name().name() != "mdscript")
+                        & (anc.tag_name().name() != "cues")
+                        & (anc.tag_name().name() != "cue")
+                    {
+                        for child in anc.descendants() {
+                            if let Some(attr) = child.attribute("name") {
+                                let item = CompletionItem {
+                                    label: attr.to_owned(),
+                                    ..CompletionItem::default()
+                                };
+                                if !namespace.contains(&item) {
+                                    namespace.push(item);
+                                }
+                            }
+                        }
+                    }
+                    if let Some(attr) = anc.attribute("name") {
                         let item = CompletionItem {
                             label: attr.to_owned(),
                             ..CompletionItem::default()
@@ -52,28 +58,9 @@ pub fn simple_complete(params: CompletionParams) -> Vec<CompletionItem> {
                         }
                     }
                 }
-            }
-        }
-        if (node.tag_name().name() != "mdscript")
-            & (node.tag_name().name() != "cues")
-            & (node.tag_name().name() != "cue")
-            & (node.tag_name().name() != "actions")
-            & (node.tag_name().name() != "")
-        {
-            info!(
-                "node.range():{:?} name: {:?}",
-                node.range(),
-                node.tag_name().name()
-            );
-            if node.range().end >= byte_position {
-                info!("loop broken ");
                 break;
             }
         }
-
-        // let pos = doc.text_pos_at(node.range().starat);
-        // if pos.row as u64 == params.text_document_position.position.line {
-        // }
     }
 
     namespace
