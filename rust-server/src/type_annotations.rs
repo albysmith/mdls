57c364c423e08df2ccf4f8cc6df6149880a998a2
albysmith <48844001+albysmith@users.noreply.github.com>
2020-05-24 18:48:08 -0500

    type annotations

--- rust-server/src/type_annotations.rs
+++ rust-server/src/type_annotations.rs
@@ -1,11 +1,36 @@
 use crate::scriptproperties::Datatypes;
 
 pub struct Event {
-	name: String,
-	description: String,
-	object: Option<Vec<Datatypes>>,
-	param: Option<Vec<Datatypes>>,
-	param1: Option<Vec<Datatypes>>,
-	param2: Option<Vec<Datatypes>>, 
-	param3: Option<Vec<Datatypes>>
+	pub id: String,
+	pub description: String,
+	pub object: Option<Vec<Datatypes>>,
+	pub param: Option<Vec<Datatypes>>,
+	pub param1: Option<Vec<Datatypes>>,
+	pub param2: Option<Vec<Datatypes>>, 
+	pub param3: Option<Vec<Datatypes>>
+}
+
+pub struct EventList {
+	pub events: Vec<Event>
+}
+
+
+pub struct Method {
+	pub id: String,
+	pub description: String,
+	pub output: Vec<Output>
+}
+
+pub struct MethodList {
+	methods: Vec<Method>
+}
+
+pub struct Output {
+	pub attr: String,
+	pub datatype: Option<Vec<Datatypes>>,
+	pub contains: Option<Vec<Datatypes>>,
+}
+
+pub fn parse_ron_files() {
+	let methods = include_str!("reference/methods.ron")
 }
