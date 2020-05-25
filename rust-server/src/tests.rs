f0ba968403af8accb655a657c85efc5037861d29
mewosmith <48843615+mewosmith@users.noreply.github.com>
2020-05-25 08:12:26 -0500

    namespace refactor

--- rust-server/src/tests.rs
+++ rust-server/src/tests.rs
@@ -27,5 +27,14 @@ mod tests {
     fn scriptproperties() {
         let scripts = ScriptProperties::new(include_str!("reference/scriptproperties.xml"));
     }
-
+    #[test]
+    //test cant fail x.x
+    // assert what namespace should be
+    // better to rewrite namespace
+    fn md_namespace_basic() {
+        let test = include_str!("reference/test_ref/md_namespace_basic.xml");
+        let namespace = parse_namespace((696, test.to_owned()));
+        println!("{:#?}", namespace);
+        assert!(namespace.is_some())
+    }
 }
